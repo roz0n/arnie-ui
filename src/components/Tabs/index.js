dacf3fced039fdee7ac4a9b2916163f4d4f479b4
Arnold Rozon <arnold@rozon.org>
2019-10-27 19:51:14 -0500

    Rewrote tabs to use hooks

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,47 +1,25 @@
 import "./Tabs.css";
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Tabs extends Component {
-  state = {
-    activeTab: 0
-  };
+const Tabs = props => {
+  const { tabs } = props;
+  const [activeTab, setActiveTab] = useState(0);
 
-  setActiveTab = (id, e) => {
-    this.setState({ activeTab: id });
-  };
-
-  renderTabsHeader() {
-    const { tabs } = this.props;
-
-    return (
+  return (
+    <article className="Tabs-container">
       <article className="tabs-header">
         {tabs.map(tab => (
           <div
-            className={tab.id === this.state.activeTab ? "tab active" : "tab"}
-            onClick={e => this.setActiveTab(tab.id, e)}
+            className={tab.id === activeTab ? "tab active" : "tab"}
+            onClick={e => setActiveTab(tab.id, e)}
           >
             {tab.title}
           </div>
         ))}
       </article>
-    );
-  }
-
-  renderTabsContent() {
-    const { tabs } = this.props;
-    const index = this.state.activeTab;
-
-    return <article>{tabs[index].component}</article>;
-  }
-
-  render() {
-    return (
-      <article className="Tabs-container">
-        {this.renderTabsHeader()}
-        {this.renderTabsContent()}
-      </article>
-    );
-  }
-}
+      <article>{tabs[activeTab].component}</article>
+    </article>
+  );
+};
 
 export default Tabs;
